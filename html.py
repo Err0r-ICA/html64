d99a1f92f709680a70ef16a0f300770e61b29702
*Err0r_HB* <59941931+Err0r-ICA@users.noreply.github.com>
2023-12-10 03:14:41 +0100

    Update html.py

--- html.py
+++ html.py
@@ -1,71 +1,15 @@
-# -*- coding: utf-8 -*-
-#usr/bin/env/python
-#Err0r_HB
-#Cyb3r Drag0nz Team
-
-import base64
-import os
-import platform
-import time
-import pyfiglet
-
-red = "\033[1;91m"
-green = "\033[1;92m"
-cyan = "\033[1;96m"
-
-# Clear the console screen based on the operating system 
+import base64,os,platform,time,pyfiglet
+red='\x1b[1;91m'
+green='\x1b[1;92m'
+cyan='\x1b[1;96m'
 def clear():
-    if platform.system() == "Windows":
-        os.system("cls")
-    else:
-        os.system("clear")
-
-# Print the banner
-banner = '''\033[38;5;197m              '##::::'##:'########:'##::::'##:'##:::::::
-               ##:::: ##:... ##..:: ###::'###: ##:::::::
-               ##:::: ##:::: ##:::: ####'####: ##:::::::
-               #########:::: ##:::: ## ### ##: ##:::::::
-               ##.... ##:::: ##:::: ##. #: ##: ##:::::::
-               ##:::: ##:::: ##:::: ##:.:: ##: ##:::::::
-               ##:::: ##:::: ##:::: ##:::: ##: ########:
-              \033[38;5;202m...:::::..:::::..:::::..:::::..::........::
-	
-\033[38;5;092m                           Coded By Err0r_HB
-'''
-
-# Print the progress bar with sleep in between 
+	if platform.system()=='Windows':os.system('cls')
+	else:os.system('clear')
+banner="\x1b[38;5;197m              '##::::'##:'########:'##::::'##:'##:::::::\n               ##:::: ##:... ##..:: ###::'###: ##:::::::\n               ##:::: ##:::: ##:::: ####'####: ##:::::::\n               #########:::: ##:::: ## ### ##: ##:::::::\n               ##.... ##:::: ##:::: ##. #: ##: ##:::::::\n               ##:::: ##:::: ##:::: ##:.:: ##: ##:::::::\n               ##:::: ##:::: ##:::: ##:::: ##: ########:\n              \x1b[38;5;202m...:::::..:::::..:::::..:::::..::........::\n\t\n\x1b[38;5;092m                           Coded By Err0r_HB\n"
 def main():
-    clear()
-    for progress in range(0, 101, 10):
-        clear()
-        print(f"{cyan}[{red}{progress}%{cyan}]{green}{'■' * (progress // 10)}")
-        time.sleep(0.5)
-
-    clear()
-    print(banner)
-
-    # Get the input HTML file
-    html = input(f'{cyan}[!]\033[38;5;021m Enter your html file {cyan}>>> {red}')
-    code = open(html, 'r', encoding='UTF-8').read()
-    text = code.encode('utf-8')
-    bsen = base64.b64encode(text).decode('utf-8')
-    html_encoding = f"""<!-- Encoding By Err0r_HB | Telegram : t.me/Cyb3r_Drag0nz -->
-<!-- GitHub :  https://github.com/Err0r-ICA -->
-
-<script type='text/javascript'>document.documentElement.innerHTML=atob('{bsen}');</script>
-"""
-    path = os.getcwd()
-    output_file = f"{html.replace('.html', '')}_en.html"
-    open(output_file, 'w').write(html_encoding)
-
-    # Print the progress bar again with sleep in between
-    for progress in range(0, 101, 10):
-        clear()
-        print(f"{cyan}[{red}{progress}%{cyan}]{green}{'■' * (progress // 10)}")
-        time.sleep(1)
-
-    clear()
-    print(banner)
-    print(f"\n{cyan}[+]{red} The file was encrypted and saved as:‍{green} {os.path.join(path, output_file)}\n")
-
+	D='utf-8';clear()
+	for A in range(0,101,10):clear();print(f"{cyan}[{red}{A}%{cyan}]{green}{'■'*(A//10)}");time.sleep(.5)
+	clear();print(banner);B=input(f"{cyan}[!][38;5;021m Enter your html file {cyan}>>> {red}");E=open(B,'r',encoding='UTF-8').read();F=E.encode(D);G=base64.b64encode(F).decode(D);H=f"<!-- Encoding By Err0r_HB | Telegram : t.me/Cyb3r_Drag0nz -->\n<!-- GitHub :  https://github.com/Err0r-ICA -->\n\n<script type='text/javascript'>document.documentElement.innerHTML=atob('{G}');</script>\n";I=os.getcwd();C=f"{B.replace('.html','')}_en.html";open(C,'w').write(H)
+	for A in range(0,101,10):clear();print(f"{cyan}[{red}{A}%{cyan}]{green}{'■'*(A//10)}");time.sleep(1)
+	clear();print(banner);print(f"\n{cyan}[+]{red} The file was encrypted and saved as:‍{green} {os.path.join(I,C)}\n")
 main()
